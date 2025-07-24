d225fc48fa9b1d59c4a41727f973831efe4340f8
concertypin <55056558+concertypin@users.noreply.github.com>
2025-07-24 16:42:12 +0900

    resolve some default export problem

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,38 @@
-import { createPathTransform } from "rollup-sourcemap-path-transform";
-import typescript from "@rollup/plugin-typescript";
+import { sync } from "glob"
+import { createPathTransform } from 'rollup-sourcemap-path-transform'
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import packageJson from './package.json' with { type: 'json' };
+const name = packageJson.name;
+
 
 const sourcemapPathTransform = createPathTransform({
-  prefixes: {
-    ".": `/risupect/`,
-  },
-  requirePrefix: true,
-});
+    prefixes: {
+        '.': `/${name}/`,
+    },
+    requirePrefix: true,
+})
 
 /**
  * @type {import('rollup').OutputOptions}
  */
 const outputLib = {
-  format: "es",
-  name: "risupect",
-  preserveModules: true,
-  validate: true,
-  dir: "dist",
-  sourcemap: "inline",
-  sourcemapPathTransform: sourcemapPathTransform,
+    format: 'es',
+    name: name,
+    preserveModules: true,
+    validate: true,
+    dir: "dist",
+    sourcemap: "inline",
+    sourcemapPathTransform: sourcemapPathTransform,
 };
 /**
  * @type {import('rollup').RollupOptions}
  */
 export default {
-  input: "src/index.ts",
-  output: [outputLib],
-
-  plugins: [typescript({ tsconfig: "./tsconfig.json" })],
-  external: [/@risu.*/],
+    input: sync("src/**/*.ts", { nodir: true }),
+    output: [outputLib],
+    plugins: [
+        typescript({ tsconfig: './tsconfig.json' }),
+        nodeResolve({ browser: true })
+    ],
 };
